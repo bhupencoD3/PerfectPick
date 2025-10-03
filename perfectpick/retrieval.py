e3849db69b857be9a8f5fc9cbcad5edce342a2fc
bhupencoD3 <bhupenparmar192@gmail.com>
2025-10-03 20:41:03 +0530

    changed to correct reranker model(BAAI/bge-reranker-v2-m3)

--- perfectpick/retrieval.py
+++ perfectpick/retrieval.py
@@ -20,7 +20,7 @@ class SearchResult:
 
 class ProductionHybridRetriever:
     def __init__(self, vector_store, docs_df: pd.DataFrame, 
-                 reranker_model: str = "BAAI/bge-reranker-small",
+                 reranker_model: str = "BAAI/bge-reranker-v2-m3",
                  bm25_weight: float = 0.4,
                  semantic_weight: float = 0.6):
         """
